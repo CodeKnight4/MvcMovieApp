fbcdea00793e0baf7418d13f53a863c30d368bbe
Ameer Ahmed <aahmed335467@gmail.com>
2024-08-09 15:48:39 +0100

    Add project files.

---
+++ MvcMovieAA/Data/MvcMovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovieAA.Data
+{
+    public class MvcMovieDbContext : DbContext
+    {
+        public MvcMovieDbContext (DbContextOptions<MvcMovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
